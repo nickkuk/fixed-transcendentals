24e21fef8f6b13c9c4347a2a208e851dcca51a91
Nikolai Kuklin <nickkuklin@gmail.com>
2022-08-30 20:01:49 +0400

    [WIP]

--- src/exp/rational.rs
+++ src/exp/rational.rs
@@ -21,12 +21,29 @@ pub fn checked_exp(x: Fix) -> Option<Fix> {
     if x >= UPPER_BOUND {
         return None;
     }
+    let (k, r): (Fix, FixP) = reduce_arg(x);
+    let exp_r: FixP = special_exp(r)?;
+    let exp_x: Fix = recover(k, exp_r);
+    Some(exp_x)
+}
+
+/// Returns pair (k, r) where x = k * ln(2) + r and |r| <= ln(2) / 2.
+fn reduce_arg(x: Fix) -> (Fix, FixP) {
     // k is integer from [-Fix::FRAC_NBITS, Fix::INT_NBITS - 1]
     let k: Fix = (Fix::LOG2_E * x).round();
     let r: FixP = FixP::from_num(x) - FixP::LN_2 * FixP::from_num(k);
+    (k, r)
+}
+
+/// Exponential function for r with |r| <= ln(2) / 2.
+fn special_exp(r: FixP) -> Option<FixP> {
     let rr: FixP = r * r;
     let c: FixP = r - rr * (P1 + rr * (P2 + rr * (P3 + rr * (P4 + rr * P5))));
     let exp_r: FixP = FixP::ONE + (r * c.checked_div(TWO - c)? + r);
+    Some(exp_r)
+}
+
+fn recover(k: Fix, exp_r: FixP) -> Fix {
     let shift: i32 = (FixP::FRAC_NBITS - Fix::FRAC_NBITS) as i32 - k.to_num::<i32>();
-    Some(Fix::from_bits(i128_shr(exp_r.to_bits(), shift)))
+    Fix::from_bits(i128_shr(exp_r.to_bits(), shift))
 }
